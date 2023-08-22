42fe15492fc4ef79002ee9dc873e0843c4a23ebe
dietvin <vdietric@students.uni-mainz.de>
2023-08-22 08:10:26 +0200

    Minor changes

--- stat_compare.py
+++ stat_compare.py
@@ -248,7 +248,7 @@ class StatComparer:
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Neet statistical summary</title>
             <style>
             body, h1, h2, h3, h4, p, ul, li {{
                 margin: 0;
--- summary.py
+++ summary.py
@@ -607,7 +607,7 @@ class SummaryCreator:
                         <meta charset="UTF-8">
                         <meta http-equiv="X-UA-Compatible" content="IE=edge">
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Document</title>
+                        <title>Neet summary</title>
                         <style>
                             /* Reset some default browser styles */
                             body, h1, h2, h3, h4, p, ul, li {{
